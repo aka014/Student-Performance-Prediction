1674d062105a0acb8b3310f5c148c7d27aef9a8b
aljosa <aljosa.crnogorac@gmail.com>
2025-07-13 17:15:03 +0200

    Add Lasso Regressor, small textual fixes for Ridge

--- source/lasso_regressor.py
+++ source/lasso_regressor.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+import sklearn.linear_model
+from matplotlib.widgets import Lasso
 from sklearn.compose import ColumnTransformer
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.pipeline import Pipeline
@@ -13,9 +14,9 @@ import linear_regressor as lr
 from features import FeatureList
 
 
-def train_and_evaluate_final(X_train, y_train, X_test, y_test, subject, alpha_list, num_list, preprocessors):
+def train_and_evaluate_final(X_train, y_train, X_test, y_test, subject, alpha_list, preprocessors):
     """
-    Trains and evaluates the ridge regression model through different preprocessing steps.
+    Trains and evaluates the lasso regression model through different preprocessing steps.
 
     Parameters:
         X_train (DataFrame): Feature DataFrame of training data.
@@ -24,7 +25,6 @@ def train_and_evaluate_final(X_train, y_train, X_test, y_test, subject, alpha_li
         y_test (DataFrame): Target DataFrame of test data.
         subject (string): School subject of data ("por" or "mat").
         alpha_list (List): Alpha regularization parameter of Ridge regression model.
-        num_list (List): Number of features to select.
         preprocessors (list): List of different preprocessors.
     """
 
@@ -37,17 +37,13 @@ def train_and_evaluate_final(X_train, y_train, X_test, y_test, subject, alpha_li
     for name, preprocessor in preprocessors:
 
         alpha = alpha_list.pop(0)
-        feat_num = num_list.pop(0)
 
-        # Ridge regression estimator
-        ridge = Ridge(alpha=alpha)
-        # Recursive Feature Elimination setup
-        rfe = RFE(estimator=ridge, n_features_to_select=feat_num)
+        # Lasso regression estimator
+        lasso = Lasso(max_iter=100000, tol=1e-5, alpha=alpha)
 
         pipeline = Pipeline(steps=[
             ('preprocessor', preprocessor),
-            ('feature_selection', rfe),
-            ('regressor', ridge)
+            ('regressor', lasso)
         ])
 
         pipeline.fit(X_train, y_train)
@@ -60,7 +56,7 @@ def train_and_evaluate_final(X_train, y_train, X_test, y_test, subject, alpha_li
         weights_intercept = lr.create_w_b_table(pipeline, features)
 
         # Write coefficients to CSV file
-        du.write_result(weights_intercept, f"{name}_ridge_extended_train_{alpha}_select{feat_num}", subject)
+        du.write_result(weights_intercept, f"{name}_lasso_extended_train_{alpha:.4f}", subject)
 
         # Calculate stats
         r2 = r2_score(y_test, y_pred)
@@ -70,7 +66,7 @@ def train_and_evaluate_final(X_train, y_train, X_test, y_test, subject, alpha_li
         # Append to list of results
         results.append({
             'Preprocessor': name,
-            'Model' : f"ridge_extended_train_{alpha}_select{feat_num}_mae",
+            'Model' : f"lasso_extended_train_{alpha:.4f}_mae",
             'MAE': f"{np.round(mae, 2):.2f}",
             'RMSE': f"{np.round(rmse, 2):.2f}",
             'R2': f"{np.round(r2, 2):.2f}"
@@ -85,7 +81,7 @@ def train_and_evaluate_final(X_train, y_train, X_test, y_test, subject, alpha_li
 
 def train_and_evaluate(X_train, y_train, X_test, y_test, subject, preprocessors):
     """
-    Trains and evaluates the ridge regression model through different preprocessing steps.
+    Trains and evaluates the lasso regression model through different preprocessing steps.
 
     Parameters:
         X_train (DataFrame): Feature DataFrame of training data.
@@ -96,8 +92,7 @@ def train_and_evaluate(X_train, y_train, X_test, y_test, subject, preprocessors)
         preprocessors (list): List of different preprocessors.
 
     Returns:
-        max_alpha (list): Best performing regularization value for each preprocessor.
-        best_num (list): Best performing number of features to select.
+        max_alpha (List): Best performing regularization value for each preprocessor.
     """
 
     # Create default FeatureList
@@ -105,7 +100,6 @@ def train_and_evaluate(X_train, y_train, X_test, y_test, subject, preprocessors)
 
     # To keep best results
     max_alpha = []
-    best_num = []
 
     # Used for stats storage
     results = []
@@ -114,21 +108,18 @@ def train_and_evaluate(X_train, y_train, X_test, y_test, subject, preprocessors)
 
         # Combinations of these parameters will be checked if they are optimal
         param_grid = {
-            'feature_selection__n_features_to_select': [20, 30, 40, 50, 60, 70],
-            'regressor__alpha': [ 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 100, 250, 500, 1000]
+            'regressor__alpha': np.logspace(-3, 0, num=100)
+            #'regressor__alpha': np.round(np.arange(0.001, 1.001, 0.001), 3)
         }
 
-        # Ridge regression estimator
-        ridge = Ridge()
-        # Recursive Feature Elimination setup
-        rfe = RFE(estimator=ridge)
+        # Lasso regression estimator
+        lasso = Lasso(max_iter=10000, tol=1e-4)
         # Set up Cross-Validation
         cv = KFold(n_splits=5, shuffle=True, random_state=42)
 
         pipeline = Pipeline(steps=[
             ('preprocessor', preprocessor),
-            ('feature_selection', rfe),
-            ('regressor', ridge)
+            ('regressor', lasso)
         ])
 
         # Search for optimal parameters and set scoring method
@@ -144,14 +135,13 @@ def train_and_evaluate(X_train, y_train, X_test, y_test, subject, preprocessors)
 
         # Get best performing alpha
         alpha = grid_search.best_params_['regressor__alpha']
-        # Get best performing number of features
-        num = grid_search.best_params_['feature_selection__n_features_to_select']
 
-        # print(alpha)
-        # print(num)
+        print("Iterations used:", grid_search.best_estimator_.named_steps['regressor'].n_iter_)
+
+        #print(alpha)
+        #print(num)
 
         max_alpha.append(alpha)
-        best_num.append(num)
 
         # Calculate stats
         r2 = r2_score(y_test, y_pred)
@@ -161,7 +151,7 @@ def train_and_evaluate(X_train, y_train, X_test, y_test, subject, preprocessors)
         # Append to list of results
         results.append({
             'Preprocessor': name,
-            'Model': f"ridge_train_{alpha}_select{num}_mae",
+            'Model': f"lasso_train_{alpha:.4f}_mae",
             'MAE': f"{np.round(mae, 2):.2f}",
             'RMSE': f"{np.round(rmse, 2):.2f}",
             'R2': f"{np.round(r2, 2):.2f}"
@@ -173,9 +163,9 @@ def train_and_evaluate(X_train, y_train, X_test, y_test, subject, preprocessors)
     # In the end, append every stats result to CSV file
     du.add_stats(pd.DataFrame(results), subject)
 
-    return max_alpha, best_num
+    return max_alpha
 
-def rr_test():
+def lasso_test():
     if __name__ == '__main__':
 
         data_por = du.read_csv("../data/student-por.csv")
@@ -195,15 +185,15 @@ def rr_test():
 
         # Make all preprocessors
         preprocessors = [
-            # ("no_scaling", ColumnTransformer([
-            #     ('cat', OneHotEncoder(handle_unknown='ignore', drop='if_binary'), features.categorical),
-            #     ('num', 'passthrough', features.numerical)
-            # ])),
-            #
-            # ("standard_scaling", ColumnTransformer([
-            #     ('cat', OneHotEncoder(handle_unknown='ignore', drop='if_binary'), features.categorical),
-            #     ('num', StandardScaler(), features.numerical)
-            # ])),
+            ("no_scaling", ColumnTransformer([
+                ('cat', OneHotEncoder(handle_unknown='ignore', drop='if_binary'), features.categorical),
+                ('num', 'passthrough', features.numerical)
+            ])),
+
+            ("standard_scaling", ColumnTransformer([
+                ('cat', OneHotEncoder(handle_unknown='ignore', drop='if_binary'), features.categorical),
+                ('num', StandardScaler(), features.numerical)
+            ])),
 
             ("standard_scaling_poly", ColumnTransformer([
                 ('cat', OneHotEncoder(handle_unknown='ignore', drop='if_binary'), features.categorical),
@@ -220,13 +210,16 @@ def rr_test():
         X_train_p, y_train_p, X_test_p, y_test_p = du.split_data_for_lr(data_por)
         #X_train_m, y_train_m, X_test_m, y_test_m = du.split_data_for_lr(data_mat)
 
-        max_alpha_p, feat_num_p = train_and_evaluate(X_train_p, y_train_p, X_train_p, y_train_p, 'por', preprocessors)
-        #max_alpha_m, feat_num_m = train_and_evaluate(X_train_m, y_train_m, X_train_m, y_train_m, 'mat')
+        max_alpha_p = train_and_evaluate(X_train_p, y_train_p, X_train_p, y_train_p, 'por', preprocessors)
+        # max_alpha_m = train_and_evaluate(X_train_m, y_train_m, X_train_m, y_train_m, 'mat', preprocessors)
 
 
-        train_and_evaluate_final(X_train_p, y_train_p, X_test_p, y_test_p, 'por', max_alpha_p, feat_num_p, preprocessors)
-        #train_and_evaluate_final(X_train_m, y_train_m, X_test_m, y_test_m, 'mat', max_alpha_m, feat_num_m)
+        train_and_evaluate_final(X_train_p, y_train_p, X_test_p, y_test_p, 'por', max_alpha_p, preprocessors)
+        #train_and_evaluate_final(X_train_m, y_train_m, X_test_m, y_test_m, 'mat', max_alpha_m, preprocessors)
 
 
-rr_test()
+lasso_test()
+
+# notice the difference in convergence time between scale + poly and poly +scale
+# first takes 47 iterations, while second takes 438 iterations
 
